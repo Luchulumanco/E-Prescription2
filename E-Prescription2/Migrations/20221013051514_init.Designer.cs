4a0aa4a9a887645bef130d1f352fcd6af5ce7cb2
Gontshi, Luchulumanco, (Mr) (s217774261) <s217774261@Mandela.ac.za>
2022-10-14 09:19:27 +0200

    Lotto Login

--- E-Prescription2/Migrations/20221013051514_init.Designer.cs
+++ E-Prescription2/Migrations/20221013051514_init.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_Prescription2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20221012132200_Pete")]
-    partial class Pete
+    [Migration("20221013051514_init")]
+    partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
